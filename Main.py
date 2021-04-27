2bb3905dff566359d2c2e7458bf2ecd3690b2d25
SADEGH <83290929+SDprojectGH@users.noreply.github.com>
2021-04-27 18:42:52 +0430

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,14 +7,16 @@ con = db.connect('mydb.db', isolation_level = None, check_same_thread = False)
 c = con.cursor()
 c.execute("CREATE TABLE IF NOT EXISTS list (userID PRIMARY KEY, fileID TEXT, messageID Integer, caption TEXT)")
 
-api_id = 00000 # Edit This Line ( Get api_ip from my.telegram.org )
-api_hash = 'aaaaaaaaaaaaaaaaaaaaaaaa' # Edit This Line ( Get api_hash from my.telegram.org )
-Token = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbb' # Edit This Line ( Get token from @botfather )
-app = Client('Session', api_id = api_id, api_hash = api_hash, bot_token = Token)
+api_id = 1551115
+api_hash = 'aadc10901b78df4ad6e2a94c8ab5b754'
+Token = '1753569872:AAFlMeqT89zFOSgL40FFu2p2UdXfulw_LDg'
+app = Client('Session', api_id = api_id, api_hash = api_hash, bot_token = Token, proxy = 
+            dict(hostname = '127.0.0.1', port = 1080))
+
 
 @app.on_message(filters.command(["start"]) & filters.private)
 def start(client, message):
-    text = "سلام به رباتمون خوش اومدی😃 فایلت کووو بفرست بیاد" # Start message, you can edit it.
+    text = "سلام به رباتمون خوش اومدی😃 فایلت کووو بفرست بیاد"
     app.send_message(message.chat.id, text)
 
 @app.on_message(filters.private)
@@ -23,19 +25,19 @@ def on_message(client, message):
     if message.document:
         FileInfo = message.document
         FileID = FileInfo.file_id
-        ChannelFileInfo = app.send_document('ChannelID', FileID) # Edit channel ID to your channel's username
+        ChannelFileInfo = app.send_document('attachfilenotimportantfile', FileID)
         MessageID = ChannelFileInfo.message_id
         c.execute(f'DELETE FROM list WHERE userID = {ChatID}')
-        c.execute('INSERT or IGNORE INTO list (userID, fileID, messageID, caption) VALUES (?, ?, ?, ?)',
+        c.execute('INSERT or IGNORE INTO list (userID, fileID, messageID, caption) VALUES (?, ?, ?, ?)', 
                   (ChatID, FileID, MessageID, None))
-        app.send_message(ChatID, 'پیام خود را وارد کنید') # you can edit this message
+        app.send_message(ChatID, 'پیام خود را وارد کنید')
     if message.text:
         c.execute(f"SELECT * FROM list WHERE userID = {ChatID}")
         Database = c.fetchall()
         if len(Database) > 0:
-            FileUrl = f"<a href='https://t.me/ChannelID/{Database[0][2]}'>&#160;</a>" # Edit channel iD to your channel's username
+            FileUrl = f" < a href = 'https://t.me/attachfilenotimportantfile/{Database[0][2]}' > &#160; < /a > "
             caption = FileUrl + message.text
-            app.send_message(Database[0][0], caption, disable_web_page_preview= False, parse_mode='HTML')
+            app.send_message(Database[0][0], caption, disable_web_page_preview = False, parse_mode = 'HTML')
     
 
 app.run()
