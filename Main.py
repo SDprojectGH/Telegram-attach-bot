889cb0b6882c5be3bb0e1147ed81ede46f8ba699
SADEGH <83290929+SDprojectGH@users.noreply.github.com>
2021-04-27 19:45:34 +0430

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,21 +20,52 @@ def start(client, message):
 @app.on_message(filters.private)
 def on_message(client, message):
     ChatID = message.chat.id
+    
+    def attach(CHATID, ID, num): # EDIT ChannelID to your channel's Username
+        ChannelID = "your Channel's Username"
+        if num == 1:
+            ChannelFileInfo = app.send_document(ChannelID, ID)
+        elif num == 2:
+            ChannelFileInfo = app.send_animation(ChannelID, ID)
+        elif num == 3:
+            ChannelFileInfo = app.send_voice(ChannelID, ID)
+        elif num == 4:
+            ChannelFileInfo = app.send_video(ChannelID, ID)
+        elif num == 5:
+            ChannelFileInfo = app.send_audio(ChannelID, ID)
+        MessageID = ChannelFileInfo.message_id
+        c.execute(f'DELETE FROM list WHERE userID = {CHATID}')
+        c.execute('INSERT or IGNORE INTO list (userID, fileID, messageID, caption) VALUES (?, ?, ?, ?)', 
+                  (CHATID, ID, MessageID, None))
+        app.send_message(CHATID, 'متن و بفرست ببینم') # you can edit this message
+    
     if message.document:
         FileInfo = message.document
         FileID = FileInfo.file_id
-        ChannelFileInfo = app.send_document('ChannelID', FileID) # Edit channel ID to your channel's username
-        MessageID = ChannelFileInfo.message_id
-        c.execute(f'DELETE FROM list WHERE userID = {ChatID}')
-        c.execute('INSERT or IGNORE INTO list (userID, fileID, messageID, caption) VALUES (?, ?, ?, ?)', 
-                  (ChatID, FileID, MessageID, None))
-        app.send_message(ChatID, 'پیام خود را وارد کنید') # you can edit this message
+        attach(ChatID, FileID, 1)
+        
+    elif message.animation:
+        FileID = message.animation.file_id
+        attach(ChatID, FileID, 2)
+    
+    elif message.voice:
+        FileID = message.voice.file_id
+        attach(ChatID, FileID, 3)
+    
+    elif message.video:
+        FileID = message.video.file_id
+        attach(ChatID, FileID, 4)
+    elif message.audio:
+        FileID = message.audio.file_id
+        attach(ChatID, FileID, 5)
+
+
     if message.text:
         c.execute(f"SELECT * FROM list WHERE userID = {ChatID}")
         Database = c.fetchall()
         if len(Database) > 0:
-            FileUrl = f"<a href ='https://t.me/attachfilenotimportantfile/{Database[0][2]}'>&#160;</a>" # Edit channel iD to your channel's username
-            caption = message.text + FileUrl
+            FileUrl = f" < a href = 'https://t.me/ChannelID/{Database[0][2]}' > &#160; < /a > " # Edit channel iD to your channel's username
+            caption = FileUrl + message.text
             app.send_message(Database[0][0], caption, disable_web_page_preview = False, parse_mode = 'HTML')
     
 
